0f45731a79dd1af313794bd583597140ff129b4a
komal-abjima <153979765+komal-abjima@users.noreply.github.com>
2024-02-02 18:33:56 +0530

    initial-commit(02-02-2024)

--- src/app/main/aabha/aadhar/aadhar-signin/aadhar-signin.component.ts
+++ src/app/main/aabha/aadhar/aadhar-signin/aadhar-signin.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nous-aadhar-signin',
+  standalone: true,
+  imports: [],
   templateUrl: './aadhar-signin.component.html',
   styleUrl: './aadhar-signin.component.scss'
 })
--- src/app/main/aabha/aadhar/aadhar-signup/aadhar-signup.component.ts
+++ src/app/main/aabha/aadhar/aadhar-signup/aadhar-signup.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nous-aadhar-signup',
+  standalone: true,
+  imports: [],
   templateUrl: './aadhar-signup.component.html',
   styleUrl: './aadhar-signup.component.scss'
 })
